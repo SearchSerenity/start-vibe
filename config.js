52f9176f411f36d1ab873ce3548e8784ebbe4800
SearchSerenity <sbenedetti@gmail.com>
2026-07-18 17:07:06 +0200

    Unsplash key

--- config.js
+++ config.js
@@ -1,5 +1,12 @@
 // config.js
-// Liste des raccourcis : modifiez uniquement cette section pour changer vos boutons
+
+// --- CONFIGURATION API UNSPLASH ---
+const unsplashConfig = {
+    accessKey: "RarF1jtu86lZTeQm02XH_1Vd6ijn_N_-HpOKjQuqLCc", // Remplacez ceci par votre clé d'accès (ex: "AbC123...")
+    keyword: "nature"           // Mot-clé pour le type d'image (laissez "" pour totalement aléatoire)
+};
+
+// --- RACCOURCIS ---
 const shortcutsConfig = [
     { title: "Google", url: "https://www.google.com" },
     { title: "YouTube", url: "https://www.youtube.com" },
