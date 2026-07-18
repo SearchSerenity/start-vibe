7b5332dcb87cfafad7264d3c7b5c1a95c9ce368d
SearchSerenity <sbenedetti@gmail.com>
2026-07-18 16:31:38 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+// Liste des raccourcis : modifiez uniquement cette section pour changer vos boutons
+const shortcutsConfig = [
+    { title: "Google", url: "https://www.google.com" },
+    { title: "YouTube", url: "https://www.youtube.com" },
+    { title: "Wikipédia", url: "https://fr.wikipedia.org" },
+    { title: "Infomaniak", url: "https://www.infomaniak.com" }
+];
